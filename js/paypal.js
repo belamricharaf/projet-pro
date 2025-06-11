a06a58a1fd7ac453812fc9a3ced6f62e179e5c52
belamricharaf <charafpicso22@gmail.com>
2025-06-11 21:14:46 +0100

    Update paypal.js

--- js/paypal.js
+++ js/paypal.js
@@ -1,14 +1,13 @@
+let cartTotal = 0;
+function updateCartDisplay() {
+  cartTotal = 0;
+  /* ton calcul total ici */
+}
+
 paypal.Buttons({
-  createOrder: (data, actions) => {
-    return actions.order.create({
-      purchase_units: [{
-        amount: { value: cartTotal.toFixed(2) }
-      }]
-    });
-  },
-  onApprove: (data, actions) => {
-    return actions.order.capture().then(details => {
-      window.location.href = 'thanks.html';
-    });
-  }
+  createOrder: (data, actions) => actions.order.create({
+    purchase_units: [{ amount: { value: cartTotal.toFixed(2) } }]
+  }),
+  onApprove: (data, actions) =>
+    actions.order.capture().then(() => window.location.href = 'thanks.html')
 }).render('#paypal-button-container');
