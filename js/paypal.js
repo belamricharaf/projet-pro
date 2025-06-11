fda60ca0bbc96d96cff2647571e55c7ca3ba74a3
belamricharaf <charafpicso22@gmail.com>
2025-06-11 21:17:30 +0100

    Update paypal.js

--- js/paypal.js
+++ js/paypal.js
@@ -1,13 +1,10 @@
-let cartTotal = 0;
-function updateCartDisplay() {
-  cartTotal = 0;
-  /* ton calcul total ici */
-}
-
 paypal.Buttons({
+  style: { layout: 'vertical', color: 'blue', shape: 'rect', label: 'paypal' },
   createOrder: (data, actions) => actions.order.create({
     purchase_units: [{ amount: { value: cartTotal.toFixed(2) } }]
   }),
   onApprove: (data, actions) =>
-    actions.order.capture().then(() => window.location.href = 'thanks.html')
+    actions.order.capture().then(() => {
+      window.location.href = 'thanks.html';
+    })
 }).render('#paypal-button-container');
